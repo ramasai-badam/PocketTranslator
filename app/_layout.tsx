32f2c88737ddb40cfede44e8e61254a655974670
Ramasai Badam <35661607+ramasai-badam@users.noreply.github.com>
2025-07-21 19:18:15 -0700

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,8 @@
-import { Stack } from 'expo-router';
+import { Stack } from 'expo-router'
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 export default function RootLayout() {
+  useFrameworkReady();
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
